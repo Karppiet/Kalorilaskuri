3b3f19fc29799ae9edf2486517c8fe95019c40d5
karppi <karppi@min.com>
2025-02-21 10:33:35 +0200

    html added to same form, script console log added and prevent default

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function calculate(){
-    
-}
+    event.preventDefault();
+    let weight = document.querySelector("#weight").value;
+    let length = document.querySelector("#length").value;
+    let age = document.querySelector("#age").value;
+    let sex = document.querySelector("#sex").value;
+
+    console.log(weight, length, age, sex);
+
+
+} 
