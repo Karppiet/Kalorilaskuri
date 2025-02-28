cbfea16703e9a9f9fa7380168fd8ccf150619968
karppi <karppi@min.com>
2025-02-28 15:13:18 +0200

    span elements and error paragraphs added

--- script.js
+++ script.js
@@ -7,24 +7,34 @@ function checkForm() {
   let age = document.querySelector("#age");
   let sex = document.querySelector("input[type='radio'][name=sex]:checked");
 
+  let wErr = document.querySelector("#weightErr");
+  let hErr = document.querySelector("#heightErr");
+  let aErr = document.querySelector("#ageErr");
+
   //alusteaan x
   let x = true;
   
   if (weight.value < 40 || weight.value > 199 || isNaN(weight.value) == true) {
     console.log("Painon pitää olla väliltä 40-199kg");
     weight.style.borderColor = "red";
+    wErr.innerHTML = `<p>Painon pitää olla väliltä 40-199kg</p>`;
+
     x = false;
   }
 
   if (height.value < 80 || height.value > 240 || isNaN(height.value) == true) {
     console.log("Pituus pitää olla välillä 80-240cm");
     height.style.borderColor = "red";
+    hErr.innerHTML = `<p>Pituus pitää olla välillä 80-240cm</p>`
+
     x = false;
   }
 
   if (age.value < 1 || age.value > 120 || isNaN(age.value) == true) {
     console.log("Ikä pitää olla välillä 1-120");
     age.style.borderColor = "red";
+    aErr.innerHTML = `<p>kä pitää olla välillä 1-120 vuotta</p>`
+
     x = false;
   }
 
@@ -37,6 +47,14 @@ function checkForm() {
 
   if (x == true) {
     calculateEnergy();
+    wErr.innerHTML = "";
+    hErr.innerHTML = "";
+    aErr.innerHTML = "";
+
+    weight.style.borderColor = "";
+    height.style.borderColor = "";
+    age.style.borderColor = "";
+    
   } else {
     console.log("täytä oikeat arvot");
   }
